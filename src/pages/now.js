ca300ca4f1f690bb10aadca4f1217c4d7918d54e
Nicholas Pretorius <nicholaspretorius@gmail.com>
2018-12-27 19:08:31 +0200

    Added new posts, updated 'Now'.

--- src/pages/now.js
+++ src/pages/now.js
@@ -6,12 +6,12 @@ export default () => (
         <h5>Nows.</h5> 
         <ul>
             <li>Building: <a href="https://github.com/nicholaspretorius/itemly" target="_blank" rel="noopener noreferrer">Itemly</a>: A rudimentary Todo API.</li>
-            <li>Learning: Nest.js, Typescript, Angular</li>
-            <li>Watching (Course): Angular Core Deep Dive by Angular Univesity</li>
-            <li>Reading: Born to Run by Christopher McDougall</li>
+            <li>Learning: Nest.js, Typescript, Angular, Ionic, Gatsby.</li>
+            <li>Watching (Course): Angular Core Deep Dive by Angular Univesity.</li>
+            <li>Reading: Born to Run by Christopher McDougall.</li>
             <li>Listening (Audiobook): A Clash of Kings by George R.R. Martin</li>
-            <li>Listening (Music): Zola Blood - Infinite Games</li>
-            <li>Watching (Series): Ozark. Season 2</li>
+            <li>Listening (Music): Zola Blood - Infinite Games, Moderat - Moderat II &amp; Moderat III</li>
+            <li>Watching (Series): Ozark. Season 2, Hinterland. Season 1.</li>
         </ul>
 
         <h5>Then:09/12</h5>
