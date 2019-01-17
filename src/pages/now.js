7235d81589c8b3180f35f2c3e3fc9e2b8d7811e8
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-01-17 20:10:09 +0200

    Added a new post, updated now.

--- src/pages/now.js
+++ src/pages/now.js
@@ -2,26 +2,55 @@ import React from "react";
 import Container from "../components/container";
 
 export default () => (
-    <Container>
-        <h5>Nows.</h5> 
-        <ul>
-            <li>Building: <a href="https://github.com/nicholaspretorius/itemly" target="_blank" rel="noopener noreferrer">Itemly</a>: A rudimentary Todo API.</li>
-            <li>Learning: Nest.js, Typescript, Angular, Ionic, Gatsby.</li>
-            <li>Watching (Course): Angular Core Deep Dive by Angular Univesity.</li>
-            <li>Reading: Born to Run by Christopher McDougall.</li>
-            <li>Listening (Audiobook): A Clash of Kings by George R.R. Martin</li>
-            <li>Listening (Music): Zola Blood - Infinite Games, Moderat - Moderat II &amp; Moderat III</li>
-            <li>Watching (Series): Ozark. Season 2, Hinterland. Season 1.</li>
-        </ul>
+  <Container>
+    <h5>Nows.</h5>
+    <ul>
+      <li>
+        Building:{" "}
+        <a
+          href="https://github.com/nicholaspretorius/itemly"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Itemly
+        </a>
+        : A rudimentary Todo API.
+      </li>
+      <li>Learning: React, Nest.js, Typescript, Angular, Ionic, Gatsby.</li>
+      <li>
+        Watching (Course): Angular Core Deep Dive by Angular Univesity &amp;
+        Mastering React by Mosh Hamedani
+      </li>
+      <li>Reading: Born to Run by Christopher McDougall.</li>
+      <li>Listening (Audiobook): A Storm of Swords by George R.R. Martin</li>
+      <li>Listening (Music): Overwerk: State</li>
+      <li>Watching (Series): Ozark. Season 2, Hinterland. Season 3.</li>
+    </ul>
 
-        <h5>Then:09/12</h5>
-        <ul>
-            <li>Learning: Node.js, Typescript, Azure</li>
-            <li>Reading: The Lean Startup by Eric Ries</li>
-            <li>Listening (Audiobook): In the Plex by Steven Levy</li>
-            <li>Listening (Music): The Glitch Mob</li>
-            <li>Watching (Series): Mad Men. Season 3</li>
-            <li>Watching (Course): The Complete Typescript Course by Angular University</li>
-        </ul>
-    </Container>
+    <h5>Thens</h5>
+    <ul>
+      <li>Learning: Node.js, Typescript, Azure</li>
+      <li>Reading: The Lean Startup by Eric Ries</li>
+      <li>Listening (Audiobook): In the Plex by Steven Levy</li>
+      <li>Listening (Music): The Glitch Mob</li>
+      <li>Watching (Series): Mad Men. Season 3</li>
+      <li>
+        Watching (Course): The Complete Typescript Course by Angular University
+        [Finished Dec 2018]
+      </li>
+      <li>
+        Listening (Audiobook): A Clash of Kings by George R.R. Martin [Finished
+        Dec 2018]
+      </li>
+      <li>
+        Listening (Music): Zola Blood - Infinite Games, Moderat - Moderat II
+        &amp; Moderat III
+      </li>
+      <li>Watching (Series): Hinterland. Season 1 &amp; Season 2</li>
+      <li>
+        Listening (Music): Zola Blood - Infinite Games, Moderat - Moderat II
+        &amp; Moderat III
+      </li>
+    </ul>
+  </Container>
 );
